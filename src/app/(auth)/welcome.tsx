dde2e48c8696c0bf03c1beb133bda846211c21a6
Orgitogj <orgitogjoci10@gmail.com>
2026-08-26 21:57:49 +0200

    Welcome page

--- src/app/(auth)/welcome.tsx
+++ src/app/(auth)/welcome.tsx
@@ -1,16 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View,Image } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/src/components/ScreenWrapper'
 import Typo from '@/src/components/Typo'
-
+import { spacingX, spacingY } from '@/src/constants/theme'
+import { verticalScale } from '@/utils/styling'
+import { colors } from '@/src/constants/theme'
 const Welcome = () => {
   return (
     <ScreenWrapper>
-      <Typo size={30} fontWeight={"700"}color='red'>Welcome page</Typo>
+      <View style={styles.container}>
+        <View>
+          <TouchableOpacity style={styles.loginButton}>
+            <Typo fontWeight={"500"}>Sign in</Typo>
+
+          </TouchableOpacity>
+          <Image source={require('../../../assets/images/welcome.png')}
+          style={styles.welcomeImage}
+          resizeMode="contain"/>
+        </View>
+        <View style={styles.footer}>
+          <View style={{alignItems:"center"}}>
+            <Typo size={30} fontWeight={"800"}>
+              Always take control
+            </Typo>
+            <Typo size={30} fontWeight={"800"}>
+             of your finances
+            </Typo>
+          </View>
+          <View style={{alignItems:"center",gap:2}}>
+            <Typo size={17} color={colors.textLight}>
+              Financed must be arranged to set a better
+            </Typo>
+            <Typo size={17} color={colors.textLight}>
+              lifestyle in future
+            </Typo>
+          </View>
+          <View style={styles.buttonContainer}></View>
+        </View>
+      </View>
     </ScreenWrapper>
   )
 }
 
 export default Welcome
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+container:{
+  flex:1,
+  justifyContent:'space-between',
+  paddingTop:spacingY._7,
+},
+welcomeImage:{
+  width:"100%",
+  height:verticalScale(300),
+  alignSelf:"center",
+  marginTop:verticalScale(100),
+},
+loginButton:{
+  alignSelf:"flex-end",
+  marginRight:spacingX._20,
+},
+footer:{
+  backgroundColor:colors.neutral900,
+  alignItems:"center",
+  paddingTop:verticalScale(30),
+  paddingBottom:verticalScale(45),
+  gap:spacingY._20,
+  shadowColor:"white",
+  shadowOffset:{width:0,height:-10},
+  elevation:10,
+  shadowRadius:25,
+  shadowOpacity:0.15,
+
+},
+buttonContainer:{
+  width:"100%",
+  paddingHorizontal:spacingX._25,
+},
+
+
+})
