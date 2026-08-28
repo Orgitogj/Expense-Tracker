f1d87c0e1bc1c3b60a0f3e888c66c1d4b89788e9
Orgitogj <orgitogjoci10@gmail.com>
2026-08-29 00:40:06 +0200

    Home tab

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useAuth } from '@/context/authContext'
+import { signOut } from 'firebase/auth';
+import { auth } from '@/config/firebase';
+import Button from '@/src/components/Button';
+import { colors } from '@/src/constants/theme';
+import Typo from '@/src/components/Typo';
+import ScreenWrapper from '@/src/components/ScreenWrapper';
 
 const Home = () => {
+  const {user}=useAuth();
+  console.log("user:",user);
+  const handleLogout=async()=>{
+    await signOut(auth);
+  };
   return (
-    <View>
-      <Text>Home</Text>
+    
+    <ScreenWrapper>
+      <Typo>Home</Typo>
+      <Button onPress={handleLogout}>
+        <Typo color={colors.black}>Logout</Typo>
+      </Button>
       
-    </View>
+    </ScreenWrapper>
   )
 }
 
