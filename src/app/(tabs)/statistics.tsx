7c2207a3255a21e678dbd856e2af37e865bafcae
Orgitogj <orgitogjoci10@gmail.com>
2026-08-29 00:40:28 +0200

    Statistics tab

--- src/app/(tabs)/statistics.tsx
+++ src/app/(tabs)/statistics.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ScreenWrapper from '@/src/components/ScreenWrapper'
 
 const Statistics = () => {
   return (
-    <View>
+    <ScreenWrapper>
       <Text>Statistics</Text>
-    </View>
+    </ScreenWrapper>
   )
 }
 
