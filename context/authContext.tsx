6a2934f87a4f4c44dd6c32b21f86bf20cbf7522e
Orgitogj <orgitogjoci10@gmail.com>
2026-08-28 23:45:02 +0200

    Auth fix

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -26,6 +26,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
           email: firebaseUser.email,
           name: firebaseUser.displayName,
         });
+        updateUserData(firebaseUser.uid);
 
         router.replace("/(tabs)");
       } else {
