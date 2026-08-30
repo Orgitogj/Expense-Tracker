e0c5980e14617626df7f5293eac236a759b71fcb
Orgitogj <orgitogjoci10@gmail.com>
2026-08-30 20:21:41 +0200

    Updated tab layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,20 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import CustomTabs from '@/src/components/CustomTabs'
 
 const _layout = () => {
-  return 
-    <Tabs tabBar={CustomTabs} screenOptions={{headerShown:false}}>
-
-      <Tabs.Screen name="index"/>
-      <Tabs.Screen name="statistics"/>
-      <Tabs.Screen name="wallet"/>
-      <Tabs.Screen name="profile"/>
+  return (
+    <Tabs tabBar={CustomTabs} screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="statistics" />
+      <Tabs.Screen name="wallet" />
+      <Tabs.Screen name="profile" />
     </Tabs>
-  
+  )
 }
 
 export default _layout
-
-const styles = StyleSheet.create({})
